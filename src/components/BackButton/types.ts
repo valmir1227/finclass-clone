93ad5e8bd46555b458f058c9f1d81724d904de21
Valmir Almeida <68277445+valmir1227@users.noreply.github.com>
2022-04-02 16:54:01 -0300

    Input Components

---
+++ src/components/BackButton/types.ts
@@ -0,0 +1,6 @@
+export interface Props {
+    onPress?: () => void;
+
+
+
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import BackButton from "./components/BackButton";
 import Button from "./components/Button";
+import Dots from "./components/Dots";
+import Input from "./components/Input";
 import Separator from "./components/Separator";
 import Text from "./components/Text";
 import { Containter } from "./styles";
@@ -7,9 +10,17 @@ import { Containter } from "./styles";
 const App: React.FC = () => {
   return (
     <Containter>
+      <Separator height={40} />
+      <BackButton />
       <Text>Hello</Text>
-      <Separator height={20} />
+      <Separator height={40} />
       <Button textType="bold" title="Assine Agora"></Button>
+      <Separator height={40} />
+      <Input name="Email" error="N tem email" />
+      <Separator height={40} />
+
+      <Input secureTextEntry name="Senha" error="Senha inválida" />
+      <Dots amount={10}></Dots>
     </Containter>
   );
 };
